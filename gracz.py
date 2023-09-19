185a7ead524e46648ad3e1dc7804a2e9c27d930c
MateuszJankos <jankowskim1002@gmail.com>
2023-09-19 19:49:52 +0200

    dodajemy gracza

---
+++ gracz.py
@@ -0,0 +1,14 @@
+import math 
+import random
+
+class Gracz:
+    def __init__(self, letter):
+        # letter znaczy x lub o 
+        self.letter = letter
+
+        def zrob_ruch(self, gra):
+            pass
+
+class Losowykomputerowygracz(gracz):
+
+    #https://www.youtube.com/watch?v=8ext9G7xspg&t=2153s
